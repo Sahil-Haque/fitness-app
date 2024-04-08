40c195c306877a50564a6f1fcd2ef172895af70c
Sahil-Haque <shaqu009@gold.ac.uk>
2024-04-09 00:34:26 +0100

    setting up homepage

--- app/index.jsx
+++ app/index.jsx
@@ -1,12 +1,14 @@
-import { View, Text} from 'react-native'
+import { View, Text, Image} from 'react-native'
 import React from 'react'
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import { StatusBar } from 'react-native-web';
 
 
 export default function Index() {
     return (
-        <View className="flex-1 justify-center items-center">
-        <Text> Hello world </Text>
+        <View className="flex-1 flex justify-end">
+        <StatusBar style="light" />
+        <Image className="h-full w-full absolute" source={require('../assets/images/homepage.jpg')} />
         </View>
     )
 }
