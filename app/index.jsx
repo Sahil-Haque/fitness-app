713cd5908cb68cd715f5b3d15c7cd078efe1f52e
Sahil-Haque <shaqu009@gold.ac.uk>
2024-04-08 23:56:46 +0100

    installed react native responsive screen and imported its width and height percentage commands

--- app/index.jsx
+++ app/index.jsx
@@ -1,9 +1,11 @@
 import { View, Text} from 'react-native'
 import React from 'react'
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+
 
 export default function Index() {
     return (
-        <View>
+        <View className="flex-1 justify-center items-center">
         <Text> Hello world </Text>
         </View>
     )
