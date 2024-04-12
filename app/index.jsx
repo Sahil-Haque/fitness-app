4937c63046bef1512152192b16773b16dbbbf5b1
Sahil-Haque <shaqu009@gold.ac.uk>
2024-04-12 16:29:16 +0100

    interactive button

--- app/index.jsx
+++ app/index.jsx
@@ -1,4 +1,4 @@
-import { View, Text, Image} from 'react-native'
+import { View, Text, Image, TouchableOpacity} from 'react-native'
 import React from 'react'
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 import { StatusBar } from 'react-native-web';
@@ -9,6 +9,32 @@ export default function Index() {
         <View className="flex-1 flex justify-end">
         <StatusBar style="light" />
         <Image className="h-full w-full absolute" source={require('../assets/images/homepage.jpg')} />
+        
+        <linearGradient
+         colors={['transparent', '#18181b']}
+         style={{width: wp(100), height: hp(70)}}
+         start={{x: 0.5, y:0}}
+         end={{x:0.5, y:0.7}}
+         className='flex justify-end pb-12 space-y-8'
+         >
+         <view className='flex items-center'>
+            <Text style={{fontSize: hp(5)}} className="text-white font-bold tracking-wide">
+               <Text className="text-rose-600"> Welcome </Text> 
+            </Text>
+         </view>   
+
+         <View>
+            <TouchableOpacity
+                style={{height: hp(7), width: wp(80)}}
+                className="bg-rose-600 flex items-center justify-center mx-auto rounded-full border-[2px] border-neutral-200 "
+            >
+                <Text  style={{fontSize: hp(3)}} className="text-white font-bold tracking-widest">
+                    Get started
+                </Text>
+            </TouchableOpacity>
+         </View>
+        </linearGradient>
         </View>
+
     )
 }
