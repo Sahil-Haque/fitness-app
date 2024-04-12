b576e26b468050524f7280038780840dcd62e0af
Sahil-Haque <shaqu009@gold.ac.uk>
2024-04-12 17:14:58 +0100

    added animations on text

--- app/index.jsx
+++ app/index.jsx
@@ -2,7 +2,8 @@ import { View, Text, Image, TouchableOpacity} from 'react-native'
 import React from 'react'
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 import { StatusBar } from 'react-native-web';
-
+import { LinearGradient } from 'expo-linear-gradient';
+import Animated, { FadeIn, FadeInDown, FadeOut } from 'react-native-reanimated';
 
 export default function Index() {
     return (
@@ -10,20 +11,26 @@ export default function Index() {
         <StatusBar style="light" />
         <Image className="h-full w-full absolute" source={require('../assets/images/homepage.jpg')} />
         
-        <linearGradient
+        {/* for the home page
+            touchable opacity command allows text to be interacted upon
+            mx refers to all margins
+            created a gradient for visual effect
+        */}
+        <LinearGradient
          colors={['transparent', '#18181b']}
-         style={{width: wp(100), height: hp(70)}}
+         style={{width: wp(100), height: hp(30)}}
          start={{x: 0.5, y:0}}
          end={{x:0.5, y:0.7}}
+         
          className='flex justify-end pb-12 space-y-8'
          >
-         <view className='flex items-center'>
+         <Animated.View entering={FadeInDown.delay(100)} className='flex items-center'>
             <Text style={{fontSize: hp(5)}} className="text-white font-bold tracking-wide">
                <Text className="text-rose-600"> Welcome </Text> 
             </Text>
-         </view>   
+         </Animated.View>   
 
-         <View>
+         <Animated.View entering={FadeInDown.delay(250).springify()}>
             <TouchableOpacity
                 style={{height: hp(7), width: wp(80)}}
                 className="bg-rose-600 flex items-center justify-center mx-auto rounded-full border-[2px] border-neutral-200 "
@@ -32,8 +39,9 @@ export default function Index() {
                     Get started
                 </Text>
             </TouchableOpacity>
-         </View>
-        </linearGradient>
+         </Animated.View>
+        </LinearGradient>
+   
         </View>
 
     )
