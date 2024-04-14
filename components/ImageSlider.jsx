5495c53f586689e92d476d72e431d78314d4b134
Sahil-Haque <shaqu009@gold.ac.uk>
2024-04-14 20:29:03 +0100

    adding dummy images to test image slider

--- components/ImageSlider.jsx
+++ components/ImageSlider.jsx
@@ -1,9 +1,28 @@
 import { View, Text } from 'react-native';
 import React from 'react';
 import carousel, {ParallaxImage} from 'react-native-snap-carousel';
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import { sliderImages } from '../constants';
 
 export default function ImageSlider(){
     return(
-       <Text>fgsdg</Text>
+       <Carousel 
+            data={sliderImages}
+            loop={true}
+            autoplay={true}
+            renderItem={ItemCard}
+            hasParallaxImages={true}
+            sliderWidth={wp(100)}
+            firstItem={1}
+            autoplayInterval={3000}
+            itemWidth={wp(100)-70}
+            slideStyle={{display: 'flex', alignItems: 'center'}}
+       />
+    )
+}
+
+const ItemCard = ()=>{
+    return(
+        <Text> Testing </Text>
     )
 }
---
+++ constants/index.js
@@ -0,0 +1,4 @@
+export const sliderImages = [
+    require('../assets/images/homepage.jpg'),
+    require('../assets/images/icon.png'),
+]
